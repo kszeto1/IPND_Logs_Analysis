5ca1c0fe31d13c4b7fbb47813b9748b98f2ee449
Kenny Szeto <kenny.szeto1@gmail.com>
2017-10-10 22:19:15 -0700

    None

---
+++ logsanalysis.py
@@ -0,0 +1,47 @@
+import psycopg2
+
+# Connect to database named 'news'
+
+def fetch_results(query):
+    db = psycopg2.connect("news")
+    c = db.cursor()
+    c.execute(query)
+    results = c.fetchall()
+    print results
+    c.close()
+
+"""CREATE VIEW most_viewed_articles as
+    SELECT articles.title, count(log.path) as views
+        FROM articles, log
+        WHERE log.path LIKE '%' || articles.slug
+        GROUP BY articles.title
+        ORDER BY views desc;
+        
+    CREATE VIEW most_popular_author as
+    SELECT authors.name, count(log.path) as author_views
+        FROM authors, articles, log
+        WHERE status != '404 NOT FOUND' 
+        AND articles.author = authors.id
+        AND log.path LIKE '%' || articles.slug || '%'
+        GROUP BY authors.name
+        ORDER BY author_views desc;
+        
+    CREATE VIEW """
+
+
+
+def most_viewed():
+    """What are the most popular three articles of all time?"""
+    print "The three most popular articles of all time:"
+    query1 = "SELECT * FROM most_viewed_articles limit 3;"
+    import pdb
+    pdb.set_trace()
+    fetch_results(query1)
+
+def most_popular_author():
+    """Who are the most popular article authors of all time?"""
+
+def most_error_day():
+    """On which days did more than 1% of requests lead to errors?"""
+
+most_viewed()
