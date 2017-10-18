e578cea831ac92ad0c0be970ff6b7aec259797d3
Kenny Szeto <kenny.szeto1@gmail.com>
2017-10-17 19:42:44 -0700

    Verify PEP8 for Udacity Review

--- logsanalysis.py
+++ logsanalysis.py
@@ -25,7 +25,7 @@ def most_viewed():
     query1 = "SELECT * FROM most_viewed_articles limit 3;"
     most_viewed_results = fetch_results(query1)
     for line in most_viewed_results:
-        print ('"' + str(line[0]) + '"' + ' - ' + str(line[1]) + ' views')
+        print('"' + str(line[0]) + '"' + ' - ' + str(line[1]) + ' views')
     print '\n'
 
 
@@ -34,7 +34,7 @@ def most_popular_author():
     query2 = "SELECT * FROM most_popular_author;"
     most_popular_results = fetch_results(query2)
     for line in most_popular_results:
-        print ('"' + str(line[0]) + '"' + ' - ' + str(line[1]) + ' views')
+        print('"' + str(line[0]) + '"' + ' - ' + str(line[1]) + ' views')
     print '\n'
 
 
@@ -43,8 +43,8 @@ def most_error_day():
     print "On which days did more than 1% of requests lead to errors?" + '\n'
     query3 = "SELECT * FROM day_with_most_errors;"
     error_results = fetch_results(query3)
-    print (str(error_results[0][0]) + ' -- ' + str(error_results[0][1]) +
-           ' errors')
+    print(str(error_results[0][0]) + ' -- ' + str(error_results[0][1]) +
+          ' errors')
     print '\n'
 
 
